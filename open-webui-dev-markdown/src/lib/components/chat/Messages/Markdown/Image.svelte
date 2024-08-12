d2f3330f56715e3d17caa6b1afe382a638c6dbe5
Yifan <yche0158@uni.sydney.edu.au>
2024-08-12 12:15:02 +1000

    main

---
+++ open-webui-dev-markdown/src/lib/components/chat/Messages/Markdown/Image.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Image from '$lib/components/common/Image.svelte';
+	import { getAstNode } from 'svelte-exmarkdown';
+	const astContext = getAstNode();
+</script>
+
+<Image src={$astContext.properties.src} alt={$astContext.properties.alt} />
