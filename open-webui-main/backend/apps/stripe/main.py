6defb97eb5ede3c914eeafa0a4aa79b33b8ec0e9
zzha0927 <zzha0927@uni.sydney.edy.au>
2024-09-13 13:37:22 +0800

    modify time type

--- open-webui-main/backend/apps/stripe/main.py
+++ open-webui-main/backend/apps/stripe/main.py
@@ -6,7 +6,7 @@ from fastapi.responses import RedirectResponse
 from apps.webui.models.users import Users
 from flask import Flask, redirect, jsonify, request
 import logging
-from datetime import datetime, timedelta
+import datetime
 
 stripe.api_key = 'sk_test_51PpnBARwKnsYpxFvqOIE6TwPUD4MPyHODJVOcnlsqrJbD8U82aN98ZUwu5NmtXAHuMyQKjPORI089WcNT9d4du6300KTgiURES'
 webhook_secret = 'whsec_45695097e5d997dbbb477f49b5f9224400d1b5764b9eb0acbd85e3a310a1a0be'
@@ -28,6 +28,7 @@ log = logging.getLogger(__name__)
 @app.post('/stripe')
 async def stripe_webhook(request: Request):
     global customer_email, price, price_id, product, product_id, status, orvip , processing_subscription_update
+
     # log.warning("Received Stripe webhook")
     payload = await request.body()
     sig_header = request.headers.get('Stripe-Signature')
@@ -121,30 +122,32 @@ async def stripe_webhook(request: Request):
 
 async def save_to_database():
     global customer_email, price, product, status, orvip
+    print("save_to_database start")
 
-    #不记录持续时间改为记录订阅开始时间和结束时间
-    ##由于创建，更新，删除皆为立即操作所以start_time获取今天日期即可
-    start_time = datetime.now().date()
-    end_time = datetime.now().date()
-    print(f"data information: {customer_email},{start_time},{end_time},{orvip}")
+    now = datetime.datetime.now()
+    start_time = now.date()
+    start_timestamp = now.timestamp()
+    int_start_time = int(start_timestamp)
+    end_time = now.date()
+
     try:
         user = Users.get_user_by_email(customer_email)
 
-        #如果user在数据库中存在，只会发生更新或者删除订阅
         if user:
-            #如果orvip为true则表示更新订阅
+            #如果orvip为true则表示更新,添加订阅
             if orvip:
                 if product == "open webui ultra":
-                    end_time = start_time + timedelta(days=365)
+                    end_time = start_time + datetime.timedelta(days=365)
                 elif product == "open webui pro":
-                    end_time = start_time + timedelta(days=30)
+                    end_time = start_time + datetime.timedelta(days=30)
                 elif product == "open webui mini":
-                    end_time = start_time + timedelta(days=7)
-
+                    end_time = start_time + datetime.timedelta(days=7)
+                end_datetime = datetime.datetime.combine(end_time, datetime.datetime.min.time())
+                int_end_time = int(end_datetime.timestamp())
                 updated_user = Users.update_user_by_id(user.id, {
                     "subscription_status": status,
-                    "subscription_start_time": start_time,
-                    "subscription_end_time":end_time,
+                    "subscription_start_time": int_start_time,
+                    "subscription_end_time":int_end_time,
                     "subscription_product":product,
                     "orvip": orvip,
                 })
@@ -156,9 +159,11 @@ async def save_to_database():
                 print(f"data time: {start_time},{end_time}")
             #如果orvip为false则表示删除用户的订阅状态
             else:
+                end_timestamp = now.timestamp()
+                int_end_time = int(end_timestamp)
                 updated_user = Users.update_user_by_id(user.id, {
-                    "subscription_status": status,
-                    "subscription_end_time": end_time,
+                    "subscription_status": int_start_time,
+                    "subscription_end_time": int_end_time,
                     "orvip": orvip,
                 })
                 if updated_user:
@@ -166,7 +171,6 @@ async def save_to_database():
                 else:
                     print(f"Failed to update subscription for user: {user.email}")
                 print(f"data time: {start_time},{end_time}")
-        #如果用户在数据库中不存在，只会发生添加订阅操作
         else:
             # if product == "open webui ultra":
             #     end_time = start_time + timedelta(days=365)
